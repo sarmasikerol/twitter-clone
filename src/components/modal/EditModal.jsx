2d17f724cee3c72240d8dcb82efc618b34d5eeb1
sarmasikerol <sarmasikerol@gmail.com>
2025-01-16 15:02:26 +0300

    revision added

--- src/components/modal/EditModal.jsx
+++ src/components/modal/EditModal.jsx
@@ -1,26 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from ".";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../firebase";
+import { toast } from "react-toastify";
+import uploadToStorage from "../../firebase/uploadToStorage";
 
-const EditModal = ({ isOpen, close }) => {
+const EditModal = ({ isOpen, close, tweet }) => {
+  const [isPicDeleting, setIsPicDeleting] = useState(false);
+
+  // dökümanı güncelle
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // inputlardaki verilere eriş
+    const text = e.target[0].value;
+    const file = e.target[1].files?.[0];
+
+    // güncellenicek dökümanın referansını al
+    const tweetRef = doc(db, "tweets", tweet.id);
+
+    let updatedData = { textContent: text, isEdited: true };
+
+    // eğer kullanıcı resmi silmek istiyorsa
+    if (isPicDeleting) {
+      updatedData.imageContent = null;
+      setIsPicDeleting(false);
+    }
+
+    if (file) {
+      // yeni resim seçildiyse
+      const imageUrl = await uploadToStorage(file);
+      updatedData.imageContent = imageUrl;
+    }
+
+    await updateDoc(tweetRef, updatedData)
+      .then(() => {
+        toast.success("İçerik güncellendi");
+
+        close();
+      })
+      .catch(() => {
+        toast.error("Bir sorun oluştu");
+      });
+  };
+
   return (
     <Modal isOpen={isOpen} close={close}>
-      <div className="flex flex-col gap-3">
-        <h1 className="text-3xl">Şifreni mi unuttun?</h1>
+      <h1 className="text-2xl">Tweet'i düzenle</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col mt-10">
+        <label>İçeriği Değiştir</label>
+        <input defaultValue={tweet.textContent} type="text" className="mt-3" />
 
-        <p className="text-gray-400">
-          Email adresine bir şifre sıfırlama bağlatısı göndericez
-        </p>
+        <label className="mt-10 mb-2">Fotoğraf Ekle / Değiştir</label>
 
-        <input type="text" className="mt-5" />
+        {tweet.imageContent ? (
+          <button
+            onClick={() => setIsPicDeleting(true)}
+            className="bg-orange-500"
+          >
+            {" "}
+            Resmi Kaldır
+          </button>
+        ) : (
+          ""
+        )}
 
-        <button type="submit" className="bg-white text-black rounded-full mt-8">
-          Mail Gönder
-        </button>
-
-        <button type="button" onClick={close}>
-          İptal
-        </button>
-      </div>
+        <div className="flex justify-end gap-5 mt-10">
+          <button
+            onClick={close}
+            type="button"
+            className="bg-gray-500 hover:bg-gray-600 transition"
+          >
+            Vazgeç
+          </button>
+          <button className="bg-blue-500 hover:bg-blue-600">Kaydet</button>
+        </div>
+      </form>
     </Modal>
   );
 };
--- src/components/post/Buttons.jsx
+++ src/components/post/Buttons.jsx
@@ -1,3 +1,4 @@
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
 import React from "react";
 import {
   FaRegComments,
@@ -5,8 +6,25 @@ import {
   FaRegHeart,
   FaShareNodes,
 } from "react-icons/fa6";
+import { auth, db } from "../../firebase";
+import { FaHeart } from "react-icons/fa";
 
-const Buttons = () => {
+const Buttons = ({ tweet }) => {
+  // oturumu açık olan kullanıcı bu tweet i likeladı mı?
+  const isLiked = tweet?.likes?.includes(auth.currentUser.uid);
+
+  const toggleLike = async () => {
+    // güncellenecek dökümanın referansını al
+    const tweetRef = doc(db, "tweets", tweet.id);
+
+    // user id'sini likes dizisine ekle
+    await updateDoc(tweetRef, {
+      likes: isLiked
+        ? arrayRemove(auth.currentUser.uid)
+        : arrayUnion(auth.currentUser.uid),
+    });
+  };
+
   return (
     <div className="flex justify-between items-center">
       <div className="p-3 rounded-full cursor-pointer transition hover:bg-blue-400/40">
@@ -17,8 +35,12 @@ const Buttons = () => {
         <FaRetweet />
       </div>
 
-      <div className="p-3 rounded-full cursor-pointer transition hover:bg-blue-400/40">
-        <FaRegHeart />
+      <div
+        onClick={toggleLike}
+        className="p-3 rounded-full cursor-pointer transition hover:bg-blue-400/30 flex items-center gap-2"
+      >
+        {isLiked ? <FaHeart className="text-red-500" /> : <FaRegHeart />}
+        {tweet?.likes?.length}
       </div>
 
       <div className="p-3 rounded-full cursor-pointer transition hover:bg-blue-400/40">
